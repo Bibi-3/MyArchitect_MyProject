5216048c35d1df99ccd39c4ec6ed4c2af66d2934
Bibi-3 <lopes.silva.gabriele@escola.pr.gov.br>
2025-06-24 08:41:16 -0300

    alteração

--- main.js
+++
@@ -1,8 +0,0 @@
-// Aqui você pode adicionar funcionalidades JavaScript mais avançadas conforme necessário
-document.getElementById("clienteBtn").addEventListener("click", function() {
-  alert("Você escolheu a opção 'Sou Cliente'.");
-});
-
-document.getElementById("profissionalBtn").addEventListener("click", function() {
-  alert("Você escolheu a opção 'Sou Profissional'.");
-});
